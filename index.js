38d065ddae96e7529a947659ee3e6e88296d9336
Debbie30 <cherotichdebby11@gmail.com>
2024-03-28 03:14:54 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function reducer(batteries) {
+    return batteries.reduce((total, battery) => total + battery, 0);
+}
+
+// Example usage:
+const batteries = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = reducer(batteries);
+console.log(totalBatteries); // Output: 31
+
+
